0cf6bb6c2ff3dfb34c877c5657a0564570dd2944
mbelenluna <mariabelenluna18@gmail.com>
2023-06-30 01:18:27 -0300

    form

--- script.js
+++ script.js
@@ -170,4 +170,41 @@ window.onload = function () {
         values.innerHTML = "Click on each value to learn more.";
     }   
 }
+emailjs.init('53Mb68rS7ENJ7t_lr');
 
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      
+      // these IDs from the previous steps
+      emailjs.sendForm('service_4thavy7', 'template_bfqz58e', this)
+          .then(function() {
+              console.log('SUCCESS!');
+          }, function(error) {
+              console.log('FAILED...', error);
+          });
+  });
+}
+
+//const sendEmail = () => {
+//   let data = {
+//     from_name : document.getElementById("name").value,
+//     email_id : document.getElementById("email").value,
+//     service_container : containerResults,
+//     file : input,
+// }
+// emailjs.send("service_byiyb7y", "template_r27difz", data).then(function (response) {
+//     modal.style.display = "block";
+//     modalBody.innerText = "Thank you for your inquiry! An agent will be in touch with you shortly.";
+//     finalResult.classList.add("hide");
+//     let startOver = document.createElement("button");
+//     startOver.classList.add("start-over");
+//     startOver.innerText = "START OVER";
+//     results.appendChild(startOver);
+//     startOver.onclick = () => {
+//         document.location.reload();
+//     }
+// });
+//}
