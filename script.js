bb58a804987bc3809a7683726906e401234005a4
mbelenluna <mariabelenluna18@gmail.com>
2023-07-18 02:01:41 -0300

    live chat

--- script.js
+++ script.js
@@ -174,13 +174,3 @@ window.onload = function () {
   }
 }
 
-//Tawk chat
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/64b618d394cf5d49dc643a99/1h5jl1v48';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
